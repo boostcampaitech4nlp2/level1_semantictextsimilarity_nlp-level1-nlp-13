8f68ba67c0495c40fbb17ba3eeb361c2c6b03f2d
intrandom5 <random_5@naver.com>
2022-11-01 14:08:45 +0000

    add unsupervised SimCSE

--- models/auto_models.py
+++ models/auto_models.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch
 
 class SBERT_base_Model(nn.Module):
-    def __init__(self, model_name):
+    def __init__(self, model_name, dropout_prob):
         super(SBERT_base_Model, self).__init__()
-        self.bert = AutoModel.from_pretrained(model_name)
+        self.bert = AutoModel.from_pretrained(model_name, hidden_dropout_prob=dropout_prob)
         self.linear = nn.Linear(self.bert.config.hidden_size*2, 1)
         self.similarity = nn.CosineSimilarity()
 
@@ -19,9 +19,9 @@ class SBERT_base_Model(nn.Module):
 
 
 class BERT_base_Model(nn.Module):
-    def __init__(self, model_name):
+    def __init__(self, model_name, dropout_prob):
         super(BERT_base_Model, self).__init__()
-        self.bert = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1)
+        self.bert = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1, hidden_dropout_prob=dropout_prob)
         self.linear = nn.Linear(self.bert.config.hidden_size, 1)
         self.similarity = nn.CosineSimilarity(dim=-1)
 
@@ -32,9 +32,9 @@ class BERT_base_Model(nn.Module):
     
 
 class BERT_base_NLI_Model(nn.Module):
-    def __init__(self, model_name):
+    def __init__(self, model_name, dropout_prob):
         super(BERT_base_NLI_Model, self).__init__()
-        self.bert = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1)
+        self.bert = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1, hidden_dropout_prob=dropout_prob)
         self.sigmoid = nn.Sigmoid()
         
     def forward(self, src_ids):
@@ -45,9 +45,9 @@ class BERT_base_NLI_Model(nn.Module):
     
 
 class MLM_Model(nn.Module):
-    def __init__(self, model_name):
+    def __init__(self, model_name, dropout_prob):
         super(MLM_Model, self).__init__()
-        self.bert = RobertaForMaskedLM.from_pretrained(model_name)
+        self.bert = RobertaForMaskedLM.from_pretrained(model_name, hidden_dropout_prob=dropout_prob)
         self.softmax = nn.LogSoftmax(dim=-1)
 
     def forward(self, src_ids):
@@ -55,3 +55,19 @@ class MLM_Model(nn.Module):
         outputs = self.softmax(outputs)
         
         return outputs
+
+
+class SimCSE(nn.Module):
+    def __init__(self, model_name, dropout_prob):
+        super(SimCSE, self).__init__()
+        self.bert = AutoModel.from_pretrained(model_name, hidden_dropout_prob=dropout_prob)
+        self.cosine_similarity = nn.CosineSimilarity()
+        
+    def forward(self, src_ids):
+        outputs1 = self.bert(src_ids).pooler_output
+        outputs2 = self.bert(src_ids).pooler_output
+        score = self.cosine_similarity(outputs1.unsqueeze(1), outputs2.unsqueeze(0))
+        
+        return outputs1, score
+        
+    
